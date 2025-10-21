9a84ac5349d4ede81bbe8eb327218ba3e2c1bb6a
Lyche <caroxmeme@gmail.com>
2025-10-21 11:12:02 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/tokenbrowser/main.go
@@ -0,0 +1,23 @@
+// cmd/tokenbrowser/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "tokenbrowser/internal/tokenbrowser"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := tokenbrowser.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
